61ce4cb2d8a5754912b677fa996771a8e7f58d56
Robin Norwood <robin@acloudguru.net>
2017-07-02 14:15:29 -0500

    Add upload portfolio script

---
+++ upload-portfolio-lambda.py
@@ -0,0 +1,19 @@
+import boto3
+from botocore.client import Config
+import StringIO
+import zipfile
+import mimetypes
+
+s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+
+portfolio_bucket = s3.Bucket('portfolio.robinnorwood.info')
+build_bucket = s3.Bucket('portfoliobuild.robinnorwood.info')
+
+portfolio_zip = StringIO.StringIO()
+build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+
+with zipfile.ZipFile(portfolio_zip) as myzip:
+    for nm in myzip.namelist():
+        obj = myzip.open(nm)
+        portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
