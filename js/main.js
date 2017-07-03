3cc2bb6e551cb0103476bb08419ab38b79b199da
Robin Norwood <robin@acloudguru.net>
2017-07-03 08:07:41 -0500

    Configure webpack and a package.json

---
+++ js/main.js
@@ -0,0 +1,5 @@
+import 'react-dom'
+
+console.log("Loaded react-dom");
+
+console.log("Webpack works");
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  entry: './js/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['react']
+        }
+      }
+    }]
+  }
+}
